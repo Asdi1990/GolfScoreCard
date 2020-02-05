6f3d6b2ac4e74a89fde441a8f8205103788ce108
Asdi1990 <asad.abbas2284@gmail.com>
2020-02-05 20:35:00 +0500

    A test project for making up my profile

---
+++ app/src/main/java/com/tadevelopers/golfscorecard/Hole.java
@@ -0,0 +1,31 @@
+package com.tadevelopers.golfscorecard;
+
+/**
+ * Created by Asad Abbas on 30-Jan-20.
+ */
+
+public class Hole {
+    private String mLabel;
+    private int mStrokeCount;
+
+    public Hole(String label, int strokeCount) {
+        mLabel = label;
+        mStrokeCount = strokeCount;
+    }
+
+    public String getLabel() {
+        return mLabel;
+    }
+
+    public void setLabel(String label) {
+        mLabel = label;
+    }
+
+    public int getStrokeCount() {
+        return mStrokeCount;
+    }
+
+    public void setStrokeCount(int strokeCount) {
+        mStrokeCount = strokeCount;
+    }
+}
